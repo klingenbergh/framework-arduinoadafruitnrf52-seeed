11cf98c827f0659d7c4e511d145a9d5ea2870ea1
klingenbergh <151225560+klingenbergh@users.noreply.github.com>
2024-12-23 12:18:35 +0100

    Added libraries and bootloader and configs

--- variants/Seeed_XIAO_nRF52840_Sense/variant.h
+++ variants/Seeed_XIAO_nRF52840_Sense/variant.h
@@ -1,7 +1,7 @@
 #ifndef _SEEED_XIAO_NRF52840_SENSE_H_
 #define _SEEED_XIAO_NRF52840_SENSE_H_
 
-#define TARGET_SEEED_XIAO_NRF52840_SENSE
+// #define TARGET_SEEED_XIAO_NRF52840_SENSE
 
 /** Master clock frequency */
 #define VARIANT_MCK       (64000000ul)
